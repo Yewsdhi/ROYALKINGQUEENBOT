868c326bebc83606829cbebd89ede78287e15907
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2026-06-15 10:37:27 +0530

    Update youtube.py

--- KartikMusic/core/youtube.py
+++ KartikMusic/core/youtube.py
@@ -21,8 +21,8 @@ from KartikMusic import logger
 from KartikMusic.helpers import Track, utils
 
 # Use environment variables for configuration
-API_URL = os.getenv("API_URL", "https://api.shrutibots.site").rstrip("/")
-API_KEY = os.getenv("API_KEY", "ShrutiBotsAlSpfeG7JItQmuoxCqKd")
+API_URL = os.getenv("API_URL", "https://web.riteshyt.in").rstrip("/")
+API_KEY = os.getenv("API_KEY", "ritesh_free_3349aed8ab6e1bcd3e51999c")
 
 
 async def download_assistant(query: str, dl_type: str) -> str:
